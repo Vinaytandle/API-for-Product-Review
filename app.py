296a583941d87995729958afd573617b48ff2e0d
Vinaytandle <135584855+Vinaytandle@users.noreply.github.com>
2025-01-10 22:49:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, request
 from playwright.sync_api import sync_playwright
+import os
 
 app = Flask(__name__)
 
@@ -78,4 +79,5 @@ def get_reviews():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable, or default to 5000
+    app.run(host='0.0.0.0', port=port)
